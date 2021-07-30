2a1d6c8956f4431dead65b4d74bf6ef64423f0db
mihailinpk <49496789+mihailinpk@users.noreply.github.com>
2021-07-30 11:30:50 +0300

    test commit from home computer

--- src/main/java/su/myspringapps/Lords_and_planets/LordsAndPlanetsApplication.java
+++ src/main/java/su/myspringapps/Lords_and_planets/LordsAndPlanetsApplication.java
@@ -1,13 +1,20 @@
 package su.myspringapps.Lords_and_planets;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LordsAndPlanetsApplication {
+public class LordsAndPlanetsApplication implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(LordsAndPlanetsApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
     }
 
 }
